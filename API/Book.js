c321c9e4f9a8cee4b262d604f290a195983fe685
shubham2386 <shubham2386@gmail.com>
2021-09-04 11:59:02 +0530

    cannot get :(

--- API/Book.js
+++ API/Book.js
@@ -1,91 +1,204 @@
-// const BookModel = require("../schema/book");
-// const Router = require(express).Router();
+const Router = require("express").Router();
 
-// // const authorModel = require("../schema/author");
+const BookModel = require("../schema/book");
+const AuthorModel = require("../schema/author");
 
+// Route    - /book
+// Des      - To get all books
+// Access   - Public
+// Method   - GET
+// Params   - none
+// Body     - none
+Router.get("/book", async (req, res) => {
+    const getAllBooks = await BookModel.find();
+    return res.json(getAllBooks);
+});
 
-// // Route       = /book
-// // Descriptin  = to get all books
-// // Access      = Public
-// // Method      = GET    
-// // params      = none
-// // Body        = none
-// // Router.get("/book", async(request, response)=>{
-// //     const getAllBooks = await BookModel.find();
+// Route    - /book/:bookID
+// Des      - To get a book based on ISBN
+// Access   - Public
+// Method   - GET
+// Params   - bookID
+// Body     - none
+Router.get("/book/:bookID", async (req, res) => {
+    const getSpecificBook = await BookModel.findOne({
+        ISBN: req.params.bookID,
+    });
 
-// //     return response.json(getAllBooks); 
-// // });
+    if (!getSpecificBook) {
+        return res.json({
+            error: `No book found fot the ISBN of ${req.params.bookID}`,
+        });
+    }
 
-// // // Route       = /book/bookID
-// // // Descriptin  = to get a book by ISBN
-// // // Access      = Public
-// // // Method      = GET 
-// // // params      = bookID
-// // // Body        = none
-// // Router.get("/book/:bookID",(request,response)=>{
-// //     const getBook=Database.Book.filter(
-// //         (book) => book.ISBN===request.params.bookID);
-// //     response.json({book:getBook});
-// // });
+    return res.json({ book: getSpecificBook });
+});
 
-// // // Route       = /book/c/:category
-// // // Descriptin  = to get a book by type of category
-// // // Access      = Public
-// // // Method      = GET 
-// // // params      = category
-// // // Body        = none
+// Route    - /book/c/:category
+// Des      - to get a list of books based on category
+// Access   - Public
+// Method   - GET
+// Params   - category
+// Body     - none
+Router.get("/book/c/:category", async (req, res) => {
+    const getSpecificBooks = await BookModel.findOne({
+        category: req.params.category,
+    });
 
-// // Router.get("/book/c/:category",(request,response)=>{
-// //     const getCategory=Database.Book.filter((book)=>
-// //         book.category.includes(request.params.category)
-// //     );
+    if (!getSpecificBooks) {
+        return res.json({
+            error: `No book found for the category of ${req.params.category}`,
+        });
+    }
 
-// //     return response.json({book:getCategory});  
-// // });
+    return res.json({ books: getSpecificBooks });
+});
 
-// // // Route         = /book/new
-// // // Description  = to add new book
-// // // Access       = Public
-// // // Method       = POST
-// // // params       = none 
-// // // Body         = none
-// // Router.post("/book/new", async(request,response)=>{
-// //     try{
-// //     const {newBook} = request.body;
-// //     await Book.create(newBook);
-// //     return response.json({message: "book added successfully"});
-// //     }catch(error){
-// //         return response.json({message: error.message});
-// //     }
-// // });
+// Route       /book/new
+// Description add new book
+// Access      PUBLIC
+// Parameters  NONE
+// Method      POST
+Router.post("/book/new", async (req, res) => {
+    try {
+        const { newBook } = req.body;
 
-// // // Route         = /bookUpdate/:isbn
-// // // Description  = to update or  change the book contents
-// // // Access       = Public
-// // // Method       = PUT
-// // // params       = ISBN 
-// // // Body         = none
-// // Router.put("/bookUpdate/:isbn",(req,res)=>{
-// //     const {updateBook} = req.body;
-// //     const {isbn} = req.params;
-   
-// //     const book = Database.Book.map((book)=>{
-// //         if (book.ISBN === isbn){
-// //             return {...book, ...updateBook};
-// //         };
-// //         return(book);
-// //     });
-// //     return res.json(book);
-// // });
+        await BookModel.create(newBook);
+        return res.json({ message: "Book added to the database" });
+    } catch (error) {
+        return res.json({ error: error.message });
+    }
+});
 
-// // Router.delete("/book/Delete/:isbn", (req, res)=>{
-// //     const { isbn } = req.params;
+// Route           /book/updateTitle
+// Description     update title of a book
+// Access          PUBLIC
+// Parameters      isbn
+// Method          PUT
+Router.put("/book/updateTitle/:isbn", async (req, res) => {
+    const { title } = req.body.title;
 
-// //     const filterBook = Database.Book.filter((book)=> book.ISBN !== isbn );
+    const updateBook = await BookModel.findOneAndUpdate(
+        {
+            ISBN: req.params.isbn,
+        },
+        {
+            title: title,
+        },
+        {
+            new: true,
+        }
+    );
 
-// //     Database.Book = filterBook;
+    return res.json({ book: updateBook });
+});
 
-// //     return res.json(Database.Book);
-// // });
+// Route       /book/updateAuthor/:isbn
+// Description update/add new author to a book
+// Access      Public
+// Paramteters isbn
+// Method      put
 
-// module.exports = Router;
+Router.put("/book/updateAuthor/:isbn", async (req, res) => {
+    const { newAuthor } = req.body;
+    const { isbn } = req.params;
+
+    const updatedBook = await BookModel.findOneAndUpdate(
+        {
+            ISBN: isbn,
+        },
+        {
+            $addToSet: {
+                authors: newAuthor,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    const updatedAuthor = await AuthorModel.findOneAndUpdate(
+        {
+            id: newAuthor,
+        },
+        {
+            $addToSet: {
+                books: isbn,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    return res.json({
+        books: updatedBook,
+        authors: updatedAuthor,
+        message: "New author was added into the database",
+    });
+});
+
+/*
+Route               /book/delete/:isbn
+Description         delete a book
+Access              PUBLIC
+Parameters          isbn
+Method              DELETE
+*/
+Router.delete("/book/delete/:isbn", async (req, res) => {
+    const { isbn } = req.params;
+
+    const updateBookDatabase = await BookModel.findOneAndDelete({
+        ISBN: isbn,
+    });
+
+    return res.json({ books: updateBookDatabase });
+});
+
+/*
+Route                   /book/delete/author
+Description             delte an author from a book
+Access                  PUBLIC
+Parameters              id, isdn
+Method                  DELETE
+*/
+Router.delete("/book/delete/author/:isbn/:id", async (req, res) => {
+    const { isbn, id } = req.params;
+
+    //updating book database object
+    const updatedBook = await BookModel.findOneAndUpdate(
+        {
+            ISBN: isbn,
+        },
+        {
+            $pull: {
+                authors: parseInt(id),
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    const updatedAuthor = await AuthorModel.findOneAndUpdate(
+        {
+            id: parseInt(id),
+        },
+        {
+            $pull: {
+                books: isbn,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    return res.json({
+        message: "Author was deleted",
+        book: updatedBook,
+        author: updatedAuthor,
+    });
+});
+
+module.exports = Router;
